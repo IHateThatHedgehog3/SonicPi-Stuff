e31f93f0f19e1bccebe4736874cb33f5db8c1ebd
IHateThatHedgehog3 <danman524@icloud.com>
2025-02-24 10:39:57 -0800

    Create ChallengeD.rb

---
+++ ChallengeD.rb
@@ -0,0 +1,128 @@
+# CHALLENGE D: Use functions to optimize the SpongeBob theme!
+
+use_bpm 136
+use_synth :piano
+
+# DON'T TOUCH THIS LL
+live_loop:background_notes do
+  play:E3, amp: 0.25
+  sleep 1
+  play:B3, amp: 0.25
+  play:E4, amp: 0.25
+  sleep 1
+  play:B3, amp: 0.25
+  sleep 1
+  play:E4, amp: 0.25
+  sleep 1
+  
+  play:E3, amp: 0.5
+  sleep 1
+  play:B3, amp: 0.5
+  play:E4, amp: 0.5
+  sleep 1
+  play:B3, amp: 0.5
+  sleep 1
+  play:E4, amp: 0.5
+  sleep 1
+  
+  play:E3, amp: 0.75
+  sleep 1
+  play:B3, amp: 0.75
+  play:E4, amp: 0.75
+  sleep 1
+  play:B3, amp: 0.75
+  sleep 1
+  play:E4, amp: 0.75
+  sleep 1
+  
+  5.times do
+    play:E3, amp: 1
+    sleep 1
+    play:B3, amp: 1
+    play:E4, amp: 1
+    sleep 1
+    play:B3, amp: 1
+    sleep 1
+    play:E4, amp: 1
+    sleep 1
+  end
+  stop
+end
+
+# Optomize all this
+
+
+define :measure1 do
+  play :r
+  sleep 1
+  play:E4
+  play:Gs4
+  sleep 2
+  play:E5
+  play:Gs4
+  sleep 1
+end
+
+# Measure 2
+define :measure28 do
+  play:E4
+  sleep 1
+  play:E5
+  play:Gs4
+  sleep 2
+  play:E5
+  play:Gs4
+  sleep 1
+end
+
+# Measure 3
+define :measure346 do
+  play :r
+  sleep 1
+  play:E5
+  play:Gs4
+  play:B4
+  sleep 1
+  play:B4
+  sleep 1
+  play:E5
+  play:Gs4
+  sleep 1
+end
+
+# Measure 5
+define :measure57 do
+  play:B4
+  sleep 0.75
+  play:As4
+  sleep 0.25
+  play:Gs4
+  play:B4
+  sleep 0.75
+  play:Cs5
+  sleep 0.25
+  play:B4
+  sleep 1
+  play:Gs4
+  play:E5
+  sleep 1
+end
+
+live_loop :newlayer do
+  32.times do
+    sleep 1
+    sample :drum_cymbal_soft, amp: 0.4
+  end
+  stop
+end
+
+measure28
+measure346
+measure346
+measure57
+measure346
+measure57
+measure28
+
+sleep 5
+sample :vinyl_scratch
